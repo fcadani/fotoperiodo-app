c1ba2ddd432e2162ad5dbaf9ee08b9fb5b1e82d7
fcadanie <angeld.flores13@gmail.com>
2025-10-17 16:52:49 -0300

    fix: Agregado @vitejs/plugin-react para solucionar error de build

--- src/main.jsx
+++ src/main.jsx
@@ -1,23 +1,10 @@
-import React from 'react'
-import ReactDOM from "react-dom/client"
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx' // O './fotoperiodo_app.jsx' si ese es el nombre del archivo
-
-// Asegúrate de que este 'root' existe en tu index.html
-const container = document.getElementById('root');
-const root = createRoot(container);
-
-root.render(
-  <React.StrictMode>
-    {/* Aquí es donde se carga el componente principal */}
-    <App /> 
-  </React.StrictMode>
-);
-
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
