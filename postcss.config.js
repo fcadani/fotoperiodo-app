7b24af542863ef55f2de3849c5fb7793ff62c953
fcadanie <angeld.flores13@gmail.com>
2025-10-17 15:39:43 -0300

    fix: Corregida la sintaxis del plugin PostCSS/Tailwind

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,8 @@
+// postcss.config.js
 export default {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
-  },
+  plugins: [
+    // Usamos el nombre del paquete como función o directamente el string
+    'tailwindcss', 
+    'autoprefixer',
+  ],
 }
